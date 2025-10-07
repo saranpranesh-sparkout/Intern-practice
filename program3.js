8c3bd9862032ef55d17c637ebb2b8e3772404002
saranpranesh-sparkout <saranpranesh@sparkouttech.com>
2025-10-07 15:29:14 +0530

    Update program3.js

--- program3.js
+++ program3.js
@@ -1,4 +1,4 @@
-/*const readline = require("readline");
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -31,7 +31,7 @@ console.log("Result:", result);
 rl.close();
 });
 });
-});*/
+});
 
 const readline = require("readline");
 
