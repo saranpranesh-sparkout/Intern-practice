96d1a09388a80b784bac688c6962424bf6a5657c
saranpranesh-sparkout <saranjagadeesh93@gmail.com>
2025-10-29 11:20:33 +0530

    Added program7

--- program2.js
+++ program2.js
@@ -1,4 +1,3 @@
- /// var
 var x = 10;
 if(true){
     var x = 20; 
@@ -6,25 +5,19 @@ if(true){
 }
 console.log("Outside if with var:", x); 
 
-// let 
 let y = 10;
 if(true){
     let y = 20; 
     console.log("Inside if with let:", y); 
 }
 console.log("Outside if with let:", y); 
-
-// const 
 const z = 5;
 console.log("Constant z:", z);
-
-// Implicit coercion
 console.log("5" + 5);   
 console.log("5" - 2);   
 console.log("5" * "2"); 
 console.log(true + 1);  
 
-// Explicit conversion
 let strNum = "123";
 let num = Number(strNum);  
 console.log(num + 1);      
@@ -32,5 +25,6 @@ console.log(num + 1);
 let val = 100;
 let strVal = String(val);  
 console.log(strVal + 1); 
+
 
 
